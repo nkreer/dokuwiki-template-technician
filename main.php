38125e973baeebb28fc4d954e051bbcfc10a0d03
nkreer <kreergit@posteo.de>
2020-12-14 19:12:49 +0100

    Basic html structure for template
    template will now import

--- main.php
+++ main.php
@@ -5,18 +5,18 @@ if(!defined("DOKU_INC")){
     die();
 }
 
-$showSidebar = true;
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $conf["lang"]; ?>">
 
 <head>
     <meta charset="UTF-8" />
-    <title><?php tpl_pagetitle() ?> - <?php echo strip_tags($conf['title']) ?></title>
-    <?php tpl_metaheaders() ?>
+    <title><?php tpl_pagetitle(); ?> - <?php echo strip_tags($conf['title']); ?></title>
+    <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+    <?php tpl_metaheaders(); ?>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
-    <?php tpl_includeFile('meta.html') ?>
+    <?php echo tpl_favicon(array('favicon', 'mobile')); ?>
+    <?php tpl_includeFile('meta.html'); ?>
 </head>
 
 <body>
@@ -27,37 +27,32 @@ $showSidebar = true;
 <div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?>">
 
 <div class="grid">
-<?php 
-    // Only include a sidebar and skiplink if its active
-    if($showSidebar){
-        ?>
-        <a href="#dokuwiki__content" class="sr-only"><?php echo $lang['skip_to_content'] ?></a>
-        <div id="dokuwiki__aside">
-            <div class="pad aside include group">
-                <?php tpl_include_page($conf['sidebar'], 1, 1) ?>
-            </div>
-        </div>
-        <?php
-    } 
-?>
 
-<!-- Page content -->
-<div id="dokuwiki__content" class="gridcolumn">
-    <?php tpl_flush(); tpl_includeFile("pageheader.html"); ?>
-    <div class="content">
-        <?php tpl_content(); ?>
+    <div id="dokuwiki__aside" class="gridcolumn">
+        <a href="#dokuwiki__content" class="sr-only"><?php echo $lang['skip_to_content'] ?></a>
+        <div class="pad aside include group">
+            <h1><?php echo strip_tags($conf['title']); ?></h1>
+            <?php tpl_include_page($conf['sidebar'], 1, 1); ?>
+        </div>
     </div>
-    <!-- Show the neccessary tools -->
-    <ul class="pagetools">
-        <?php 
-            tpl_toolsevent('usertools', [
-                tpl_action("edit", 1, "li", 1),
-                tpl_action("admin", 1, "li", 1),
-                tpl_action("login", 1, "li", 1)
-            ];
-        ?>
-    </ul>
-</div>
+
+    <!-- Page content -->
+    <div id="dokuwiki__content" class="gridcolumn">
+        <?php tpl_flush(); tpl_includeFile("pageheader.html"); ?>
+        <div class="content">
+            <?php tpl_content(); ?>
+        </div>
+        <!-- Show the neccessary tools -->
+        <ul class="pagetools">
+            <?php 
+                tpl_toolsevent('usertools', [
+                    tpl_action("edit", 1, "li", 1),
+                    tpl_action("admin", 1, "li", 1),
+                    tpl_action("login", 1, "li", 1)
+                ]);
+            ?>
+        </ul>
+    </div>
 
 </div>
 
